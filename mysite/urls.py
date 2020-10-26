6600f183d5c5e4951e1ac5ee5cc49ac1a5e243a3
mppatterson84 <mppatterson84@gmail.com>
2020-10-26 10:22:56 -0500

    modify admin title and header

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,3 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
 ]
+
+admin.site.site_header = 'Mysite Admin Panel'
+admin.site.site_title = 'Mysite Admin Panel'
